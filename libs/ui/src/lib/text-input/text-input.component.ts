9d8fbe91ba4950c627c83b526a5da4598783caca
AlviHysa <alvi.hysa@coders.al>
2022-07-07 15:35:09 +0200

    Added Text Input

--- libs/ui/src/lib/text-input/text-input.component.ts
+++ libs/ui/src/lib/text-input/text-input.component.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'trainum-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css'],
+  styleUrls: ['./text-input.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TextInputComponent implements OnInit {
+  @Input() label = ' Text Input ';
+
   constructor() {}
 
   ngOnInit(): void {}
